e376374270941daacb72f3aa6f36b77c7dbd3419
IanMarcello <ian.marcello.jada19@gmail.com>
2022-12-22 09:33:05 +0300

    Create header.blade.php

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,15 @@
+<!-- Header -->
+<div class="text-gray-500 border-b border-gray-100 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-slate-100">
+    <div class="flex justify-between h-16">
+        <div class="flex">
+            <!-- Logo -->
+            <div class="shrink-0 flex items-center">
+                <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+            </div>
+            <!-- Title -->
+            <div class="inline-flex items-center px-1 pt-1 text-md font-medium font-bold text-gray-900">
+                {{ __('POMS') }}
+            </div>
+        </div>
+    </div>
+</div>
